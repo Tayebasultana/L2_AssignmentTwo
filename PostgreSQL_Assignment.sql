16dbadd80211d9fbc091b8427246bf6f734bad23
Tayeba Sultana <sultanatayeba43@gmail.com>
2025-05-26 19:52:38 +0600

    show recent data, sighting's time of day, Delete rangers who have never sighted any species is done

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -7,6 +7,13 @@ SELECT * FROM "species"
 
 SELECT * FROM "sightings"
 
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
+('Dodo', 'Raphus cucullatus', '1598-01-01', 'Historic'),
+('Great Auk', 'Pinguinus impennis', '1750-01-01', 'Historic'),
+('Tasmanian Tiger', 'Thylacinus cynocephalus', '1800-01-01', 'Historic'),
+('Irish Elk', 'Megaloceros giganteus', '1770-01-01', 'Historic');
+
+
 -- Problem-1: Register a new ranger
 INSERT INTO rangers (name, region) VALUES ('Liam Rivers', 'Highland Valley');
 
@@ -25,3 +32,24 @@ ORDER BY r.name;
 -- Problem-5: List species that have never been sighted.
 SELECT s.common_name FROM species s
 LEFT JOIN sightings si ON s.species_id = si.species_id Where si.species_id IS NULL;
+
+-- Problem-6: Show the most recent 2 sightings.
+SELECT sp.common_name, si.sighting_time, r.name from sightings si 
+JOIN species sp ON si.species_id = sp.species_id 
+JOIN rangers r ON si.ranger_id = r.ranger_id 
+ORDER BY si.sighting_time DESC LIMIT 2;
+
+-- Problem-7: Update all species discovered before year 1800 to have status 'Historic'.
+
+-- PRoblem-8: Label each sighting's time of day as 'Morning', 'Afternoon', or 'Evening'.
+SELECT sighting_id, CASE 
+WHEN EXTRACT (HOUR FROM sighting_time) < 12 THEN 'Morning'
+WHEN EXTRACT (HOUR FROM sighting_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+ELSE 'Evening' END AS time_of_day FROM sightings;
+
+-- Problem-9: Delete rangers who have never sighted any species.
+DELETE FROM rangers WHERE ranger_id NOT IN (SELECT DISTINCT ranger_id FROM sightings);
+-- For test case this operation add some new ranger data who have never sighted any species.This is new data in rangers table.
+INSERT INTO rangers (name, region) VALUES
+('Liam Rivers', 'Misty Mountains'),
+('Maya Forest', 'Sunny Meadows');
